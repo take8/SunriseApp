54012abff6f2fa83a837981f2de94bd7e858d706
Takeya Shimizu <takeya.shimizu@sportsbull.jp>
2022-01-30 16:48:57 +0900

    Update: Add link to outlet and action

--- SunriseApp/ViewController.swift
+++ SunriseApp/ViewController.swift
@@ -9,11 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var cityNameInput: UITextField!
+    @IBOutlet weak var sunriseTimeLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func findSunrise(_ sender: Any) {
+    }
 }
 
