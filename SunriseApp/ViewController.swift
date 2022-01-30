2e3f05b60f265bf03b657ea3f251a9d103ea9da0
Takeya Shimizu <takeya.shimizu@sportsbull.jp>
2022-01-30 18:15:48 +0900

    Update: Show sunrise time to label

--- SunriseApp/ViewController.swift
+++ SunriseApp/ViewController.swift
@@ -29,11 +29,30 @@ class ViewController: UIViewController {
             let apiURL = URL(string: url)!
             let data = try Data(contentsOf: apiURL)
             let json = try JSONSerialization.jsonObject(with: data) as! [String: Any]
-            print(json)
+            // print(json)
+            let sys = json["sys"] as! [String:Any]
+            let sunrise = sys["sunrise"] as! TimeInterval
+            // print(sunrise)
+            let formattedDate = self.formatDate(dateUnix: sunrise)
+            self.sunriseTimeLabel.text = "日の出時刻: \(formattedDate)"
         } catch {
             print("error occurred...")
             self.sunriseTimeLabel.text = "サーバーに接続できません"
         }
+    }
+
+    // TODO: extensionにすべき?
+    // TODO: Anyはよろしくないはず
+    func formatDate(dateUnix: TimeInterval) -> String {
+        let date = Date(timeIntervalSince1970: dateUnix)
+
+        // NSDate型を日時文字列に変換するためのNSDateFormatterを生成
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+
+        // NSDateFormatterを使ってNSDate型 "date" を日時文字列 "dateStr" に変換
+        let dateStr: String = formatter.string(from: date)
+        return dateStr
     }
 }
 
